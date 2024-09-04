4c4c2c2fc2881dd3ec140edb6015be939ee88a76
ajkost <ajkost@gmail.com>
2024-09-04 20:50:58 +0200

    chore: refactor gitlink plugin for improved performance and readability
    - Utilized Neovim's built-in functions for better integration
    - Simplified remote selection logic
    - Improved string manipulation and variable assignments
    - Enhanced debug messaging with appropriate log levels
    - Optimized range calculation
    - Used vim.tbl_deep_extend for more robust option merging

--- lua/gitlink.lua
+++ lua/gitlink.lua
@@ -15,33 +15,19 @@ local function debug(msg)
   end
 end
 
--- split the string by the given delimiter
-local function split(str, delimiter)
-  local t = {}
-  for s in string.gmatch(str, string.format("([^%s]+)", delimiter)) do
-    table.insert(t, s)
-  end
-  return t
-end
-
--- trim the string
-local function trim(str)
-  return str:match("^%s*(.-)%s*$")
-end
-
 -- exec system command
 local function exec(str)
-  return trim(vim.fn.system(str))
+  return vim.trim(vim.fn.system(str))
 end
 
 -- get the http link based on the arguments
 local function get_http_link(remote, branch, file, range)
   local http_url
   local git_url = exec('git config --get remote.' .. remote .. '.url'):gsub('.git$', '')
-  if not string.match(git_url, "^https://") then
+  if not git_url:match('^https://') then
     git_url = git_url:gsub(':', '/'):gsub('.*@', 'https://')
   end
-  return trim(string.format('%s/blob/%s/%s%s', git_url, branch, file, range))
+  return string.format('%s/blob/%s/%s%s', git_url, branch, file, range)
 end
 
 -- main function
@@ -62,48 +48,30 @@ function M.create_git_link()
   end
   debug('Local branch: ' .. branch)
 
-  local remotes = split(M.options.remotes, ',')
+  local remotes = vim.split(M.options.remotes, ',')
   debug('Remotes: ' .. vim.inspect(remotes))
-  local remote = nil
-  -- iterate over defined remotes and find first that contains given branch
-  for _, r in ipairs(remotes) do
-    local output = exec('git branch --remotes --list ' .. string.format('%s/%s', r, branch))
-    debug('Output for remote: ' .. r .. ', branch: ' .. branch .. ': ' .. output)
-    if output ~= "" then
-      debug('Selected remote: ' .. r)
-      remote = r
-      break
-    end
-  end
+  -- select the first remote that contains the branch or nil
+  local remote = vim.tbl_filter(function(r)
+    return exec('git branch --remotes --list ' .. r .. '/' .. branch) ~= ''
+  end, remotes)[1]
 
   -- if no remote contains the current branch, link to the default remote's default branch
-  if remote == nil then
+  if not remote then
     debug('No remote branch found')
     remote = exec('git config --get checkout.defaultremote')
     debug('Default remote: ' .. remote)
-    local strings = split(exec('git symbolic-ref refs/remotes/'.. remote .. '/HEAD'), '/')
-    branch = strings[#strings]
+    branch = exec('git symbolic-ref refs/remotes/'.. remote .. '/HEAD'):match('[^/]+$')
     debug('Default branch for remote: ' .. remote .. ': ' .. branch)
   end
 
   -- get the current file path relative to the repository root
-  local file_path = vim.fn.expand('%:p'):gsub(repo_root:gsub('-', '%%-') .. '/', '')
+  local file_path = vim.fn.expand('%:p'):gsub(vim.pesc(repo_root) .. '/', '')
   debug('File path relative to the repository root: ' .. file_path)
   -- get the visual range start / end linenumbers
-  local start_line = vim.fn.line('v')
-  local end_line = vim.fn.line('.')
+  local start_line, end_line = vim.fn.line('v'), vim.fn.line('.')
   debug('Visual range: ' .. start_line .. '-' .. end_line)
 
-  local range
-
-  -- the visual range is directional, so the start_line may be higher than end_line, so swap them if that's the case
-  if start_line == end_line then
-    range = '#L' .. start_line
-  elseif start_line < end_line then
-    range = string.format('#L%s-L%s', start_line, end_line)
-  else
-    range = string.format('#L%s-L%s', end_line, start_line)
-  end
+  local range = start_line == end_line and '#L' .. start_line or string.format('#L%d-L%d', math.min(start_line, end_line), math.max(start_line, end_line))
 
   local link = get_http_link(remote, branch, file_path, range)
   -- copy the link to the clipboard
